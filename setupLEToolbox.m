3f1bffa8b4e07fd4153452ca6d0f8666bf533404
Zhenping Yin <zp.yin@whu.edu.cn>
2023-07-17 20:36:37 +0800

    support chinese char

--- setupLEToolbox.m
+++ setupLEToolbox.m
@@ -3,13 +3,17 @@ fprintf('Author: Zhenping Yin\n');
 fprintf('Email: zp.yin@whu.edu.cn\n');
 fprintf('WeChat: ZPYin07\n');
 
+% change encoding
+currentCharacterEncoding = slCharacterEncoding();
+slCharacterEncoding('UTF-8');
+
 rootDir = fileparts(mfilename('fullpath'));
 addpath(genpath(fullfile(rootDir, 'lib')));
 addpath(genpath(fullfile(rootDir, 'include')));
 
 global LEToolboxInfo
-LEToolboxInfo.programVersion = '1.2';
-LEToolboxInfo.updateDate = datenum(2022, 11, 12);
+LEToolboxInfo.programVersion = '1.3';
+LEToolboxInfo.updateDate = datenum(2023, 7, 17);
 LEToolboxInfo.institute = 'INAST';
 LEToolboxInfo.author = 'Zhenping Yin';
 LEToolboxInfo.email = 'zp.yin@whu.edu.cn';
