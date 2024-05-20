3b50b414a266c243287dc62ab851e366b9ad2f38
Zhenping Yin <zp.yin@whu.edu.cn>
2024-05-20 09:39:20 +0800

    rm promptup

--- setupLEToolbox.m
+++ setupLEToolbox.m
@@ -1,8 +1,3 @@
-fprintf('Setting up lidar evaluation toolbox!\n');
-fprintf('Author: Zhenping Yin\n');
-fprintf('Email: zp.yin@whu.edu.cn\n');
-fprintf('WeChat: ZPYin07\n');
-
 % change encoding
 currentCharacterEncoding = slCharacterEncoding();
 slCharacterEncoding('UTF-8');
@@ -20,5 +15,3 @@ LEToolboxInfo.author = 'Zhenping Yin';
 LEToolboxInfo.email = 'zp.yin@whu.edu.cn';
 LEToolboxInfo.institute_logo = fullfile(rootDir, 'image', 'logo_cma.png');
 LEToolboxInfo.projectDir = rootDir;
-
-fprintf('Success!!!\n');
